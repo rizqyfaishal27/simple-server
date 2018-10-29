251f7c6e1d2e578a5c0e17db235487f38a677e9a
Rizqy Faishal <rizqyfaishal27@gmail.com>
2018-10-30 05:36:41 +0700

    update

--- src/main/java/app/types/Host.java
+++ src/main/java/app/types/Host.java
@@ -1,28 +1,41 @@
 package webapp.types;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "ip",
+    "npm"
+})
 public class Host {
 
-    private String npm;
+    @JsonProperty("ip")
     private String ip;
+    @JsonProperty("npm")
+    private String npm;
 
-    public Host(String ip, String npm) {
-        this.npm = npm;
-        this.ip = ip;
-    }
-
-    public String getNpm() {
-        return this.npm;
-    }
-
+    @JsonProperty("ip")
     public String getIp() {
-        return this.ip;
+        return ip;
     }
 
+    @JsonProperty("ip")
     public void setIp(String ip) {
         this.ip = ip;
     }
 
+    @JsonProperty("npm")
+    public String getNpm() {
+        return npm;
+    }
+
+    @JsonProperty("npm")
     public void setNpm(String npm) {
         this.npm = npm;
     }
+
 }
